0d313b995ca79d938d183340260b2f788225a5e9
gioiascari <gioia.ascari96@gmail.com>
2022-04-26 14:57:44 +0200

    Impostazione documento

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <MyHeader />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/MyHeader.vue";
+
+export default {
+  name: "App",
+  components: {
+    MyHeader,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "@/style/utilities";
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

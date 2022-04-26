d156819d7e9b0d16966b9e89e192935c6da68e23
gioiascari <gioia.ascari96@gmail.com>
2022-04-26 16:59:10 +0200

    aggiunta 'logo' su jumbotron

--- src/components/JumbotronHeader.vue
+++ src/components/JumbotronHeader.vue
@@ -2,7 +2,13 @@
   <div class="jumbotron">
     <div class="container">
       <div class="row">
-        <div class="col-6">sss</div>
+        <div class="col-6">
+          <!-- NavBar -->
+          <div id="logo">
+            <h1><span>NEX</span>GEN</h1>
+          </div>
+          <!-- /NavBar -->
+        </div>
         <div class="col-6"></div>
       </div>
     </div>
@@ -16,11 +22,29 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import "@/style/mixin";
+
 .jumbotron {
   height: 600px;
   background-image: url("../assets/img/bg-2.jpg");
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
+}
+#logo {
+  color: white;
+  padding-top: 50px;
+  h1 {
+    font-size: 20px;
+  }
+}
+#logo span {
+  font-size: 20px;
+  padding-left: 50px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  color: $bg-btn;
+  background-color: #52b9bb60;
+  border-radius: 40px 0 0 40px;
 }
 </style>
